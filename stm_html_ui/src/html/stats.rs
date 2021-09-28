b16a99a772be3d0f92b280fdebb6ea5c39d62c60
rimutaka <max@onebro.me>
2021-09-28 09:28:42 +0000

    Stats page changes to match new combined stats struct

--- stm_html_ui/src/html/stats.rs
+++ stm_html_ui/src/html/stats.rs
@@ -9,30 +9,19 @@ use serde_json::Value;
 pub(crate) struct Stats {
     stm_stats_dev_job_counts: Value,
     stm_stats_repo_job_counts: Value,
-    stm_stats_report_success_counts: Value,
-    stm_stats_report_generation_time_avg: Value,
     stm_stats_report_fail_counts: Value,
 }
 
 pub(crate) async fn html(config: &Config, html_data: HtmlData) -> Result<HtmlData, ()> {
     // get the data from ES
-    let stm_stats_dev_job_counts =
-        elastic::get_stm_stats(&config.es_url, "stm_stats_dev_job_counts", 60);
-    let stm_stats_repo_job_counts =
-        elastic::get_stm_stats(&config.es_url, "stm_stats_repo_job_counts", 60);
-    let stm_stats_report_success_counts =
-        elastic::get_stm_stats(&config.es_url, "stm_stats_report_success_counts", 12);
-    let stm_stats_report_generation_time_avg =
-        elastic::get_stm_stats(&config.es_url, "stm_stats_report_generation_time_avg", 12);
-    let stm_stats_report_fail_counts =
-        elastic::get_stm_stats(&config.es_url, "stm_stats_report_fail_counts", 12);
+    let stm_stats_dev_job_counts = elastic::get_stm_stats(&config.es_url, "stm_stats_dev_job_counts", 60);
+    let stm_stats_repo_job_counts = elastic::get_stm_stats(&config.es_url, "stm_stats_repo_job_counts", 60);
+    let stm_stats_report_fail_counts = elastic::get_stm_stats(&config.es_url, "stm_stats_report_fail_counts", 12);
 
     // run the queries concurrently
     let jobs = vec![
         stm_stats_dev_job_counts,
         stm_stats_repo_job_counts,
-        stm_stats_report_success_counts,
-        stm_stats_report_generation_time_avg,
         stm_stats_report_fail_counts,
     ];
     let mut response = join_all(jobs).await;
@@ -49,14 +38,6 @@ pub(crate) async fn html(config: &Config, html_data: HtmlData) -> Result<HtmlDat
             .pop()
             .expect("stm_stats_repo_job_counts ES query failed")
             .expect("stm_stats_repo_job_counts ES query failed"),
-        stm_stats_report_success_counts: response
-            .pop()
-            .expect("stm_stats_report_success_counts ES query failed")
-            .expect("stm_stats_report_success_counts ES query failed"),
-        stm_stats_report_generation_time_avg: response
-            .pop()
-            .expect("stm_stats_report_generation_time_avg ES query failed")
-            .expect("stm_stats_report_generation_time_avg ES query failed"),
         stm_stats_report_fail_counts: response
             .pop()
             .expect("stm_stats_report_fail_counts ES query failed")
