bedd170e78c776cc9bec6776e0efd39b55f3d91c
rimutaka <max@onebro.me>
2021-11-18 19:53:25 +0000

    Switched to lambda-debug-proxy-client
    * updated Cargo.toml
    * removed redundant code from `main`

--- stm_html_ui/src/main.rs
+++ stm_html_ui/src/main.rs
@@ -1,13 +1,12 @@
 #[cfg(not(debug_assertions))]
 use lambda_runtime::handler_fn;
+use lambda_runtime::Error;
 
 mod config;
 mod elastic;
 mod handler;
 mod html;
 mod search_log;
-
-pub(crate) type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
@@ -32,117 +31,36 @@ async fn main() -> Result<(), Error> {
 /// not be deployed to Lambda.
 #[cfg(debug_assertions)]
 mod proxy {
-    use lambda_runtime::Context;
+    use lambda_debug_proxy_client::{get_input, send_output};
+    use lambda_runtime::Error;
     use rusoto_core::region::Region;
-    use rusoto_sqs::{DeleteMessageRequest, ReceiveMessageRequest, SendMessageRequest, Sqs, SqsClient};
-    use serde::Deserialize;
-    use serde_json::Value;
     use tracing::info;
-
-    pub(crate) type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
     const AWS_REGION: Region = Region::UsEast1; // replace with your preferred region
     const REQUEST_QUEUE_URL_ENV: &str = "STM_HTML_LAMBDA_PROXY_REQ"; // add your queue URL there
     const RESPONSE_QUEUE_URL_ENV: &str = "STM_HTML_LAMBDA_PROXY_RESP"; // add your queue URL there
 
-    #[derive(Deserialize, Debug)]
-    struct RequestPayload {
-        pub event: Value,
-        pub ctx: Context,
-    }
-
     pub(crate) async fn run() -> Result<(), Error> {
-        #[cfg(debug_assertions)]
+        let request_queue_url = std::env::var(REQUEST_QUEUE_URL_ENV)
+            .expect(&format!("Missing {} env var with the SQS request queue URL", REQUEST_QUEUE_URL_ENV))
+            .trim()
+            .to_string();
+
+        let response_queue_url = std::env::var(RESPONSE_QUEUE_URL_ENV)
+            .expect(&format!("Missing {} env var with the SQS request queue URL", RESPONSE_QUEUE_URL_ENV))
+            .trim()
+            .to_string();
+
         loop {
             // get event and context details from the queue
-            let (payload, receipt_handle) = get_input().await?;
-            info!("New msg");
+            let (payload, receipt_handle) = get_input(&AWS_REGION, &request_queue_url).await?;
+            info!("New msg arrived");
             // invoke the handler
             let response = crate::handler::my_handler(payload.event, payload.ctx).await?;
 
             // send back the response and delete the message from the queue
-            send_output(response, receipt_handle).await?;
+            send_output(response, receipt_handle, &AWS_REGION, &request_queue_url, &response_queue_url).await?;
             info!("Msg sent");
         }
-    }
-
-    /// Read a message from the queue and return the payload as Lambda structures
-    async fn get_input() -> Result<(RequestPayload, String), Error> {
-        let client = SqsClient::new(AWS_REGION);
-
-        // start listening to the response
-        loop {
-            let resp = client
-                .receive_message(ReceiveMessageRequest {
-                    max_number_of_messages: Some(1),
-                    queue_url: std::env::var(REQUEST_QUEUE_URL_ENV)
-                        .expect(&format!("Missing {} env var with the SQS request queue URL", REQUEST_QUEUE_URL_ENV))
-                        .trim()
-                        .to_string(),
-                    wait_time_seconds: Some(20),
-                    ..Default::default()
-                })
-                .await?;
-
-            // wait until a message arrives or the function is killed by AWS
-            if resp.messages.is_none() {
-                continue;
-            }
-
-            // an empty list returns when the queue wait time expires
-            let msgs = resp.messages.expect("Failed to get list of messages");
-            if msgs.len() == 0 {
-                continue;
-            }
-
-            // the message receipt is needed to delete the message from the queue later
-            let receipt_handle = msgs[0]
-                .receipt_handle
-                .as_ref()
-                .expect("Failed to get msg receipt")
-                .to_owned();
-
-            // convert JSON encoded body into event + ctx structures as defined by Lambda Runtime
-            let body = msgs[0].body.as_ref().expect("Failed to get message body");
-            let payload: RequestPayload = serde_json::from_str(body).expect("Failed to deserialize msg body");
-
-            return Ok((payload, receipt_handle));
-        }
-    }
-
-    /// Send back the response and delete the message from the queue.
-    async fn send_output(response: Value, receipt_handle: String) -> Result<(), Error> {
-        let client = SqsClient::new(AWS_REGION);
-
-        let response = response.to_string();
-
-        // SQS messages must be shorter than 262144 bytes
-        if response.len() < 262144 {
-            client
-                .send_message(SendMessageRequest {
-                    message_body: response,
-                    queue_url: std::env::var(RESPONSE_QUEUE_URL_ENV)
-                        .expect(&format!("Missing {} env var with the SQS response queue URL", RESPONSE_QUEUE_URL_ENV))
-                        .trim()
-                        .to_string(),
-                    ..Default::default()
-                })
-                .await?;
-        } else {
-            println!("Message size: {}B, max allowed: 262144B", response.len());
-        }
-
-        // delete the request msg from the queue so it cannot be replayed again
-        client
-            .delete_message(DeleteMessageRequest {
-                queue_url: std::env::var(REQUEST_QUEUE_URL_ENV)
-                    .expect(&format!("Missing {} env var with the SQS request queue URL", REQUEST_QUEUE_URL_ENV))
-                    .trim()
-                    .to_string(),
-                receipt_handle,
-            })
-            .await?;
-
-        Ok(())
     }
 }
