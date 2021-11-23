f4128375abc182810c923ad15559b74a899a44bd
rimutaka <max@onebro.me>
2021-11-23 19:45:48 +0000

    Return soft 404 on User Not Found #21
    * added a check to dev_profile and gh_login_profile
    * dev_profile is untested

--- stm_html_ui/src/html/dev_profile.rs
+++ stm_html_ui/src/html/dev_profile.rs
@@ -10,11 +10,26 @@ pub(crate) async fn html(config: &Config, owner_id: String, html_data: HtmlData)
     let query =
         elastic::add_param(elastic::SEARCH_DEV_BY_DOC_ID, owner_id.clone(), &config.no_sql_string_invalidation_regex);
 
+    let devs = elastic::search(&config.es_url, &config.dev_idx, Some(&query)).await?;
+
+    // default response code
+    let mut http_resp_code = 404_u32;
+
+    // check if the dev profile was found to return 200
+    if let Some(v) = devs.get("hits") {
+        if let Some(v) = v.get("hits") {
+            if v.is_array() && v.get(0).is_some() {
+                // content found - return 200
+                http_resp_code = 200;
+            }
+        }
+    }
+
     let html_data = HtmlData {
-        devs: Some(elastic::search(&config.es_url, &config.dev_idx, Some(&query)).await?),
+        devs: Some(devs),
         template_name: "dev.html".to_owned(),
         ttl: 3600,
-        http_resp_code: 200,
+        http_resp_code,
         owner_id_str: Some(owner_id),
         meta_robots: Some("noindex".to_owned()),
         ..html_data
--- stm_html_ui/src/html/gh_login_profile.rs
+++ stm_html_ui/src/html/gh_login_profile.rs
@@ -4,23 +4,31 @@ use crate::elastic;
 use tracing::info;
 
 /// Returns the developer profile. Expects a valid login
-pub(crate) async fn html(
-    config: &Config,
-    login: String,
-    html_data: HtmlData,
-) -> Result<HtmlData, ()> {
+pub(crate) async fn html(config: &Config, login: String, html_data: HtmlData) -> Result<HtmlData, ()> {
     info!("Generating html-dev");
-    let query = elastic::add_param(
-        elastic::SEARCH_ENGINEER_BY_LOGIN,
-        login.clone(),
-        &config.no_sql_string_invalidation_regex,
-    );
+    let query =
+        elastic::add_param(elastic::SEARCH_ENGINEER_BY_LOGIN, login.clone(), &config.no_sql_string_invalidation_regex);
+
+    let devs = elastic::search(&config.es_url, &config.dev_idx, Some(query.as_str())).await?;
+
+    // default response code
+    let mut http_resp_code = 404_u32;
+
+    // check if the dev profile was found to return 200
+    if let Some(v) = devs.get("hits") {
+        if let Some(v) = v.get("hits") {
+            if v.is_array() && v.get(0).is_some() {
+                // content found - return 200
+                http_resp_code = 200;
+            }
+        }
+    }
 
     let html_data = HtmlData {
-        devs: Some(elastic::search(&config.es_url, &config.dev_idx, Some(query.as_str())).await?),
+        devs: Some(devs),
         template_name: "dev.html".to_owned(),
         ttl: 3600,
-        http_resp_code: 200,
+        http_resp_code,
         login_str: Some(login),
         ..html_data
     };
