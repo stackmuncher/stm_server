bbecbf6af95e84b9b016846d4fce6f59d5c42c05
rimutaka <max@onebro.me>
2021-12-25 20:26:38 +0000

    Added `stm_stats_deletion_queue_counts` to Stats page

--- stm_html_ui/src/html/stats.rs
+++ stm_html_ui/src/html/stats.rs
@@ -11,6 +11,7 @@ pub(crate) struct Stats {
     stm_stats_repo_job_counts: Value,
     stm_stats_report_fail_counts: Value,
     stm_stats_contributor_counts: Value,
+    stm_stats_deletion_queue_counts: Value,
 }
 
 pub(crate) async fn html(config: &Config, html_data: HtmlData) -> Result<HtmlData, ()> {
@@ -19,6 +20,7 @@ pub(crate) async fn html(config: &Config, html_data: HtmlData) -> Result<HtmlDat
     let stm_stats_repo_job_counts = elastic::get_stm_stats(&config.es_url, "stm_stats_repo_job_counts", 60);
     let stm_stats_report_fail_counts = elastic::get_stm_stats(&config.es_url, "stm_stats_report_fail_counts", 12);
     let stm_stats_contributor_counts = elastic::get_stm_stats(&config.es_url, "stm_stats_contributor_counts", 60);
+    let stm_stats_deletion_queue_counts = elastic::get_stm_stats(&config.es_url, "stm_stats_deletion_queue_counts", 60);
 
     // run the queries concurrently
     let jobs = vec![
@@ -26,6 +28,7 @@ pub(crate) async fn html(config: &Config, html_data: HtmlData) -> Result<HtmlDat
         stm_stats_repo_job_counts,
         stm_stats_report_fail_counts,
         stm_stats_contributor_counts,
+        stm_stats_deletion_queue_counts,
     ];
     let mut response = join_all(jobs).await;
     response.reverse();
@@ -49,6 +52,10 @@ pub(crate) async fn html(config: &Config, html_data: HtmlData) -> Result<HtmlDat
             .pop()
             .expect("stm_stats_contributor_counts ES query failed")
             .expect("stm_stats_contributor_counts ES query failed"),
+        stm_stats_deletion_queue_counts: response
+            .pop()
+            .expect("stm_stats_deletion_queue_counts ES query failed")
+            .expect("stm_stats_deletion_queue_counts ES query failed"),
     };
 
     // put everything together for Tera
