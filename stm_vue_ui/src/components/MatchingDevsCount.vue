da542e2839ecda01a3cd2c6c86f8b3d48f1ac7b8
rimutaka <max@onebro.me>
2022-03-31 16:46:35 +1300

    Use of Apollo cache #30

--- stm_vue_ui/src/components/MatchingDevsCount.vue
+++ stm_vue_ui/src/components/MatchingDevsCount.vue
@@ -12,14 +12,14 @@ const tech = ref(store.tech);
 
 const count = ref(0);
 
-const stackVar = () => {
+const stackVar = computed(() => {
   const stack = new Array<inpTechExperienceInterface>();
 
   tech.value.forEach((v, k) => {
     stack.push({ tech: k, locBand: v.loc } as inpTechExperienceInterface);
   });
 
-  console.log("stackVar() called");
+  console.log("stackVar computed");
   console.log(stack);
 
   let x = {
@@ -27,21 +27,9 @@ const stackVar = () => {
   };
 
   return x;
-};
+});
 
-// const hasData = computed(() => {
-//   console.log(`Computed: ${tech.value.size}`);
-//   return tech.value.size > 0;
-// });
-
-// // const x = ref(false);
-
-// console.log(`Setup hasData: ${hasData.value}`);
-
-const { result, loading, error, refetch } = useQuery(
-  devCountForStack,
-  stackVar()
-);
+const { result, loading, error } = useQuery(devCountForStack, stackVar);
 
 watch(result, (value) => {
   console.log(value);
@@ -54,23 +42,23 @@ watch(store.tech, async (tNew, tOld) => {
   console.log("Watch tech (new/old)");
   console.log(tNew);
   console.log(tOld);
-
-  if (tNew.size > 0) {
-    console.log("refetching");
-    await refetch(stackVar());
-  }
 });
-
-// watch(hasData, (newHasData, oldHasData) => {
-//   console.log(`Watch hasData: ${oldHasData} -> ${newHasData}`);
-// });
 </script>
 
 <template>
-  <p>{{ tech }} / has data: {{ tech.size > 0 }}</p>
-  <p v-if="error">{{ error }}</p>
   <h6>
-    Matches: <span v-if="loading"> Loading ...</span
-    ><span v-else>{{ count }}</span>
+    <span v-if="stackVar.stack.length == 0"> Total profiles: </span>
+    <span v-else> Matching profiles: </span>
+
+    <span v-if="loading"> Loading ...</span>
+    <span v-else>{{ count }}</span>
   </h6>
+  <ul class="text-muted list-inline">
+    <li v-for="t in stackVar.stack" :key="t.tech" class="list-inline-item">
+      {{ t.tech }}
+    </li>
+  </ul>
+  <p v-if="error" class="text-danger">
+    <small>{{ error }}</small>
+  </p>
 </template>
